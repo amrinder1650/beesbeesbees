fc08ff081426debf713a7ffaf52d80b7b6c4bfab
merwhite11 <meredith.white91@gmail.com>
2021-08-30 19:44:38 -0700

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this)
+  this.age = 5;
+  this.color = 'yellow';
 };
 
+//Grub is the superclass
+
+//Bee needs to set its prototype to delegate lookups to Grub.prototype
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+Bee.prototype.job = 'keep on growing';
