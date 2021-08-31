99cbee1a5e99e1050ac8d331e8ddd9b9363b8789
merwhite11 <meredith.white91@gmail.com>
2021-08-30 19:13:27 -0700

    Complete Grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,13 @@
+//pseudoclassical -- we can add 'universal instance' props to the class constructor using the this kw .
+
 var Grub = function() {
+  //this = object.create(Grub.prototype) {}
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+};
+
+Grub.prototype.eat = function(){
+
 };
 
